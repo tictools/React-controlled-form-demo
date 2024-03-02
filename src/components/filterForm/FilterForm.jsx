189694e6b6f6218473df1889d5230317a80f93d2
tictools <jaumevillarreal@gmail.com>
2024-03-02 13:41:39 +0100

    fix: remove old code

--- src/components/filterForm/FilterForm.jsx
+++ src/components/filterForm/FilterForm.jsx
@@ -1,33 +1,3 @@
-/* 
-import { INITIAL_INPUT_VALUE } from "../../hooks";
-
-export const FilterForm = ({ filterValue, updateFilterValue }) => {
-  const onFilterChange = (event) => updateFilterValue(event.target.value);
-
-  const resetFilter = () => updateFilterValue(INITIAL_INPUT_VALUE);
-
-  return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-      className="filter-form"
-    >
-      <input
-        className="filter-form__input"
-        type="text"
-        placeholder="filter list by name..."
-        value={filterValue}
-        onChange={onFilterChange}
-      />
-      <button className="filter-form__reset" onClick={resetFilter}>
-        ⛌
-      </button>
-    </form>
-  );
-}; 
-*/
-
 import { useRef } from "react";
 import { INITIAL_INPUT_VALUE } from "../../hooks";
 
